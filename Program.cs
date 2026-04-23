70f8b82e50780d4499c32a18907afe6b930b3ce3
Taohid Hossain <htaohid31@gmail.com>
2026-04-23 20:39:33 +0600

    initialize project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+if(app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}
+
+app.MapGet("/error", () => Results.Problem());
+app.MapControllers();
+
+app.MapGet("/", () => "Working");
+app.MapGet("/error/test", () => { throw new Exception("test"); });
+
+app.Run();
