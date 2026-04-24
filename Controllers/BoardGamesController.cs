e81c8a57eedfbcead576a1115a1458138fc9a1e0
Taohid Hossain <htaohid31@gmail.com>
2026-04-24 11:01:08 +0600

    add BoardGames Controller

---
+++ Controllers/BoardGamesController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using MyBGList.Models;
+
+namespace MyBGList.Controllers;
+
+[Route("/api/v1/[controller]")]
+[ApiController]
+public class BoardGamesController(ILogger<BoardGamesController> logger)
+{
+    private readonly ILogger<BoardGamesController> _logger = logger;
+
+    [HttpGet(Name= "GetBoardGames")]
+    public IEnumerable<BoardGame> Get()
+    {
+        return new[]
+        {
+            new BoardGame
+            {
+                Id = 1,
+                Name = "Axis & Allies",
+                Year = 1981
+            },
+            new BoardGame
+            {
+                Id = 2,
+                Name = "Citadels",
+                Year = 2000
+            },
+            new BoardGame
+            {
+                Id = 3,
+                Name = "Terraforming Mars",
+                Year = 2016
+            }
+        };
+    }
+}
