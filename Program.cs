522edc5defce2b6ca7605bfebe09dd5e5879b449
Taohid Hossain <htaohid31@gmail.com>
2026-04-24 11:29:42 +0600

    add CORS

--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
+using Microsoft.AspNetCore.Cors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+    {
+    options.AddDefaultPolicy(cfg =>
+    {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]!);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(name: "AnyOrigin",
+        cfg =>
+        {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+        });
+    }
+);
 
 var app = builder.Build();
 
+app.UseCors();
 if(app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,10 +41,11 @@ else
     app.UseExceptionHandler("/error");
 }
 
-app.MapGet("/error", () => Results.Problem());
 app.MapControllers();
+app.MapGet("/error", [EnableCors("AnyOrigin")] () => Results.Problem());
 
 app.MapGet("/", () => "Working");
-app.MapGet("/error/test", () => { throw new Exception("test"); });
+app.MapGet("/error/test", [EnableCors("AnyOrigin")] () => 
+    { throw new Exception("test"); });
 
 app.Run();
